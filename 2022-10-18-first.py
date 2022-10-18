ac593050457d2410ef6258ec7eb36b45cfcc95ac
kimuJW <a01020532509@gmail.com>
2022-10-18 19:14:01 +0900

    codes

---
+++ 2022-10-18-first.py
@@ -0,0 +1,20 @@
+count=0
+N=int(input())
+n=N
+if N<0 or N>99: 
+    exit()        
+while True:     
+    a=(N%10)*10+(N//10+N%10)%10
+    count+=1
+    if a!=N:
+        N=a                  
+    if a==n:
+        break   
+print(count)
+
+
+
+
+
+   
+    
